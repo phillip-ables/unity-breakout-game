b500bea91d5e7d6cb76c098d5a61f94967296067
Phillip <phillipables1@gmail.com>
2018-01-28 01:15:27 -0500

    initialized paddlespeed and new vector3 for player position in paddles script

---
+++ BreakOut/Assets/Scripts/Paddle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+    public float paddleSpeed = 1;
+    private Vector3 playerPos = new Vector3(0, -6, 7);
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
