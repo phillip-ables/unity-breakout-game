fb396dc619c664546d795279caebbc2e1fd05449
Phillip <phillipables1@gmail.com>
2018-01-29 00:09:34 -0500

    x pos plus input of horizontal axis times paddle speed Clamped -8 and 8 into a vector three and applied to transform position

--- BreakOut/Assets/Scripts/Paddle.cs
+++ BreakOut/Assets/Scripts/Paddle.cs
@@ -8,6 +8,8 @@ public class Paddle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float xPos = transform.position.x + Input.GetAxis("Horizontal") * paddleSpeed;
+        playerPos = new Vector3(Mathf.Clamp(xPos, 8f, -8f), -6, 7);
+        transform.position = playerPos;
 	}
 }
