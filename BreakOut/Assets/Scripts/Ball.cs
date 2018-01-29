deb943e13c406ed3f73472605bf8099a9e538591
Phillip <phillipables1@gmail.com>
2018-01-29 02:13:17 -0500

    declare initial bel and RigidBody for Awake GameComponent

---
+++ BreakOut/Assets/Scripts/Ball.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public float ballInitialVel = 600f;
+    private RigidBody rb;
+
+	// Like start but before start
+	void Awake () {
+        rb = GameComponent<Rigidbody>;
+;	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
