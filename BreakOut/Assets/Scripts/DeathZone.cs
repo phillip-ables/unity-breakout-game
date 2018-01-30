6d52a0607e14e3a6dd1b81085cb20c384282b132
Phillip <phillipables1@gmail.com>
2018-01-29 20:15:35 -0500

    DeathZone script on collision LoseLife

---
+++ BreakOut/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour {
+    //this is for the water where we made the collider a trigger
+    void OnTriggerEnter()
+    {
+        GM.instance.LoseLife();
+    }
+}
